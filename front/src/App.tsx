55988bafeb82e993982ee13e45784e32dbe4ebaf
k-sugawa <sugawakumiko@gmail.com>
2024-01-16 21:25:57 +0900

    front data update

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TodoInput } from "./components/TodoInput.js";
+import { TodoList } from "./components/TodoList.js";
+import { TodoRest } from "./components/TodoRest.js";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ToDo List</h1>
+      <TodoInput todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoRest todos={todos} />
+    </>
   );
 }
-
-export default App;
---
+++ front/src/components/TodoInput.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import addBtn from "../assets/images/icon-add.svg";
+
+export const TodoInput = ({ todos, setTodos }) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputText);
+    setTodos([
+      ...todos,
+      {
+        id: uuidv4(),
+        name: inputText,
+        completed: false,
+        editing: false,
+      },
+    ]);
+    setInputText("");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="todoinput-line">
+          <input
+            type="text"
+            onChange={(e) => setInputText(e.target.value)}
+            className="todo-input"
+            value={inputText}
+            placeholder="新しいtodoを追加"
+          />
+          <button className="todo-addBtn">
+            <img src={addBtn} alt="add-icon" />
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default TodoInput;
